0dd477783b421a1ac43d63a6da1f43d2dbc89281
luanamaria06 <159254931+luanamaria06@users.noreply.github.com>
2024-11-20 14:03:24 -0300

    paginas de cadastro e de entrar

---
+++ pagina_login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Login</h2>
+        <form action="login_process.php" method="post">
+            <div class="form-group">
+                <label for="username">Usuário</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Senha</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit">Entrar</button>
+        </form>
+        <a href="cadastro.php">Não tem uma conta? Cadastre-se</a>
+    </div>
+</body>
+</html>
