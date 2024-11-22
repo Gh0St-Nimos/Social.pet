711423b8a5cbc2ad95f26d7d755b219fc53eead4
hastz142 <160739659+hastz142@users.noreply.github.com>
2024-11-22 17:59:12 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";  // aqui vai dar acesso ao htdocs do sistema principoal a fim de acessar o banco de dados...
+$username = "root";  / usuario que vamos logar
+$password = "";  // 'senha' padrão do usuario que vamos logar.      Depois era legal mudar isso, e colocar com senha padrão mesmo.
+$dbname = "COLOCA_O_NOME_DO_REPOSITORIO";  // aqui é o nome do repositorio que vamos usar, por favor modificar para o nome do banco de dados que vamos usar.
+
+// aqui crio a conexao
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// aqui checo se ta ok
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
