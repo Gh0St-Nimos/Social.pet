2e85ac615ea774a3ad4df570f99a16821940b240
hastz142 <160739659+hastz142@users.noreply.github.com>
2024-11-22 18:19:27 -0300

    Create adicionar_usuario.php

---
+++ adicionar_usuario.php
@@ -0,0 +1,23 @@
+<?php
+include 'conexao.php'; //aqui chama a conexao.php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // é importante lembrar que aqui eu pego os dados do formulario!
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $usuario_id = 1; // "por enquanto, um ID fixo de usuário, depois incluimos um login"
+
+    // inserindo no banco
+    $sql = "INSERT INTO projects (usuario, email, senha, usuario_id)
+            VALUES ('usuario', 'email', 'senha', 'usuario_id')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Novo projeto adicionado com sucesso!";
+    } else {
+        echo "Erro: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
